4654c5978402472e4b371150855e17a6d79c3735
uzinok <41639106+uzinok@users.noreply.github.com>
2023-07-16 19:37:55 +0300

    добавляет комментарии и поправляет текст

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,26 @@
-console.log('hi!xx');
-const hello = 'hello';
-let bue = 'bue';
-document.write(hello);
-class Buka {
-	constructor(){}
+function dtime(num) {
+	const date = new Date();
+
+	date.setDate(date.getDate() + num);
+	var options = {
+		month: 'long',
+		day: 'numeric',
+	};
+	document.write(date.toLocaleString("en-US", options));
 }
-// alert('hi!!!!!!!!!!!')
+
+window.addEventListener('load', function() {
+	const likes = document.querySelectorAll('.like');
+
+	for (let i = 0; i < likes.length; i++) {
+		likes[i].addEventListener('click', function() {
+			if (this.classList.contains('like--down')) {
+				this.classList.remove('like--down');
+				this.innerText = --this.innerText;
+			} else {
+				this.classList.add('like--down');
+				this.innerText = ++this.innerText;
+			}
+		});
+	}
+});
